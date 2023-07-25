8e6be949c316488394549a9e09ee2a3034dc0c6a
ISEL-47224-23DA <sindrome.gracinha@gmail.com>
2023-07-25 20:50:42 +0100

    forgot changing main route

--- src/server/server.js
+++ src/server/server.js
@@ -3,11 +3,11 @@ import client from "../whatsapp/whatsapp.js";
 import webApi from "./api/webApi.js";
 import routes from "./routes/routes.js";
 
-console.log('Starting up bot...')
+console.log("Starting up bot...");
 
 client.initialize();
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.use(routes(webApi(client)))
+app.use("/whatsapp", routes(webApi(client)));
